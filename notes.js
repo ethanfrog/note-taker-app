2c299b865505577651f310cbf0d79a5d38cc6394
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-03-16 20:55:26 -0400

    Added notes.js, added functions for reading and adding notes

---
+++ notes.js
@@ -0,0 +1,41 @@
+const util = require('util');
+const fs = require('fs');
+const uuid = require('uuid/v1');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Notes {
+  read() {
+    return readFileAsync('./db/db.json', 'utf8');
+  }
+
+  getNotes() {
+    return this.read()
+    .then((notes) => {
+      let notesArray = [].concat(JSON.parse(notes));
+      return notesArray;
+    });
+  }
+
+  write(note) {
+    return writeFileAsync('./db/db.json', JSON.stringify(note));
+  }
+
+  addNote(note) {
+    const noteTitle = note.title;
+    const noteText = note.text;
+    const noteID = uuid();
+
+    const newNote = { noteTitle, noteText, noteID };
+
+    // Get all notes, add the new note, write all the updated notes, return the newNote
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
+
+}
+
+module.exports = new Notes();
--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
