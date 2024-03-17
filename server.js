a4d755bfab11a5e2fa3275baadf501989a511b77
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-03-16 21:19:32 -0400

    Added routing middleware

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+const htmlRouteStart = require('./htmlRoutes');
+const apiRouteStart = require('./apiRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use('/', htmlRouteStart);
+app.use('/api', apiRouteStart);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
